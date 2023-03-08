32d8be3984bb91fd3c06749d4df7058bdb473ce7
Ziad Mohamed <78522322+zezo07@users.noreply.github.com>
2023-03-08 23:10:54 +0200

    Update build.gradle.kts

--- AkwamProvider/build.gradle.kts
+++ AkwamProvider/build.gradle.kts
@@ -9,5 +9,5 @@ cloudstream {
 
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "Cartoon" )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=akwam.to&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=eg1.ak4eg.cam&sz=%size%"
 }
