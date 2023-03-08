25852b11c2cd47288007f7cf613f23fc6bcc9a7a
Ziad Mohamed <78522322+zezo07@users.noreply.github.com>
2023-03-08 22:56:25 +0200

    Update build.gradle.kts
    transfer to WeCima

--- MyCimaProvider/build.gradle.kts
+++ MyCimaProvider/build.gradle.kts
@@ -9,5 +9,5 @@ cloudstream {
 
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=mycima.tv&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=wecima.tube&sz=%size%"
 }
